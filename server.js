512b6422c424ab95c6816c4aa684c72b79b0c3af
Frantz Crozilhac <fran.stacksta@gmail.com>
2020-12-30 11:32:52 +0100

    Set-up - schema creation

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-server.js
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql')
+const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name:'Helloworld',
+    fields: () =>({
+      message: {
+        type: GraphQLString,
+        resolve:()=>('Hello world')
+      }
+    })
+  })
+})
+
+app.use('/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  }),
+);
+app.listen(5000,()=>console.log('Server Running'))
